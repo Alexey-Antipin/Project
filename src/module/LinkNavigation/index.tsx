969a282b0e2b838d6010945f6783c5f3616d8913
Alexey <stierlitzotto21@gmail.com>
2022-11-11 12:46:35 +0900

    [develop] - создание news-detailed

---
+++ src/module/LinkNavigation/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Sprite } from "../../svg";
+import { LinkProps } from "../../ts";
+import "./index.scss";
+
+export const LinkNavigation: React.FC<LinkProps> = ({
+  link,
+  deeperLink,
+}) => {
+  return (
+    <div className="link__article-link">
+      <span className="link__home">
+        <Sprite id="home" home={deeperLink && "#4E64F9"} />
+
+        {!deeperLink && <div className="link__round"></div>}
+
+        <span className={`link__text ${deeperLink && "link__text--blue"}`}>
+          {link}
+        </span>
+
+        {deeperLink && (
+          <>
+            <div className="link__round"></div>
+            <span className="link__text">{deeperLink}</span>
+          </>
+        )}
+        <h1 className="link__title">{deeperLink || link}</h1>
+      </span>
+    </div>
+  );
+};
